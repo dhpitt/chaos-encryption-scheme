034edcb9fa001e7e5aee150b5b9430dc912f0ae4
dhpitt <david.h.pitt@gmail.com>
2021-01-10 12:25:53 -0500

    added attractor

--- conceptArt.py
+++ conceptArt.py
@@ -5,18 +5,21 @@ x = []
 r = []
 
 # Generate the attractor for every value of r
-for i in range(400,1600,1):
-    r.append(i*0.0025)
+for i in range(3000):
+    r.append(3.5+i*0.001)
     #attractor = logisticEq(i*0.0025,0.7,200)
     #x.append(attractor[100:])
-    x.append(logisticEq(i*0.0025,0.7,200)[100:])
+    x.append(logisticEq(3.5+i*0.001,0.7,200)[100:])
 
 ax = plt.subplot(111)
+ax.set(xlim=(3.5,3.8),ylim=(0,1))
 plt.xlabel('r')
 plt.ylabel('x')
 plt.title(r'$x_n = rx_{n-1}(1-x_{n-1})$')
 
 for i in range(len(r)):
     for j in x[i]:
-        ax.plot(r[i],j,'bo',ms=0.2)
+        ax.plot(r[i],j,'ro',ms=0.2)
+
+plt.savefig("images/bifurcation_diagram_2.png",dpi=300)
 plt.show()
