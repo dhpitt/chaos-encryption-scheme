2aea2ccc7514a6f8d10cd184c6aba0e817af389c
dhpitt <david.h.pitt@gmail.com>
2021-01-11 14:16:13 -0500

    added OTP

--- logisticEnc.py
+++ logisticEnc.py
@@ -60,11 +60,13 @@ def decryptText2(inputString,key):
 
 # Testing method 2
 
+'''
 test_input2 = input("Enter a string of ASCII characters to encrypt: ")
 outTest2 = encryptText2(test_input2,testKey)
 print("Your string encrypted by method 2: " + str(outTest2))
 print("Decrypting...")
 print("Your string decrypted by method 2: " + str(decryptText2(outTest2,testKey)))
+'''
 
 def encryptText3(inputString,key):
     # Method 3: also works on ASCII, but instead of worrying about the values
@@ -80,6 +82,7 @@ def decryptText3(inputString,key):
     decrypted = [chr(rankMap.index(ord(char))) for char in inputString]
     return ''.join(decrypted)
 
+'''
 test_input3 = input("Enter a string of ASCII characters to encrypt: ")
 outTest3 = encryptText3(test_input3,testKey)
 print("Your string encrypted by method 3: " + str(outTest3))
@@ -87,6 +90,24 @@ print("Decrypting...")
 print("Your string decrypted by method 3 with the key: " + str(decryptText3(outTest3,testKey)))
 print("Your string decrypted by method 3 with a bad key: " + str(decryptText3(outTest3,testKey2)))
 print("Your string decrypted by method 3 with another bad key: " + str(decryptText3(outTest3,testKey3)))
+'''
+
+def oneTimePadEncrypt(inputString,key):
+    # generates a logistic one-time pad of the length of the input 
+    # then adds the OTP to the input character-wise, and encodes
+    # as hex.
+    pad = logisticRankedMap(key[0],key[1],len(inputString))
+    combined = [hex(ord(char) + x)  for char,x in zip(inputString,pad)]
+    return ' '.join(combined)
+
+def oneTimePadDecrypt(inputString,key):
+    # decrypts text encrypted by the OTP method above
+    # by obtaining the duplicate one-time pad from the key.
+    split = inputString.split()
+    pad = logisticRankedMap(key[0],key[1],len(split))
+    decrypted = [chr(int(hex,16) - x) for hex,x in zip(split,pad)]
+    return ''.join(decrypted)
+
 
 # eventual image encoding, useless for now - just a code snippet that
 # imports a binary image and encodes it in base64.
@@ -96,3 +117,8 @@ image_read = image.read()
 image_64_encode = base64.encodestring(image_read)
 '''
 
+loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pharetra, dui ac cursus elementum, lorem lacus faucibus risus, vel fermentum massa diam et ipsum. Nam tincidunt enim eu erat maximus viverra. Sed id felis id orci malesuada viverra. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec tincidunt aliquam rhoncus. Sed sagittis, elit mollis vestibulum efficitur, risus libero fringilla leo, sit amet ullamcorper purus erat nec lorem. Cras cursus in nibh hendrerit commodo. Praesent eget feugiat diam. Sed placerat a sapien eget pharetra. Etiam congue enim nec neque mattis dignissim. \n Integer sit amet est id dolor mattis lacinia ac eget nisi. Sed diam urna, venenatis sed egestas vitae, gravida at purus. Nulla ac posuere nibh, et aliquam justo. Pellentesque elit odio, pharetra nec orci sed, volutpat egestas nisi. Integer et nisi vel orci eleifend mattis at in libero. Mauris iaculis id tellus tristique luctus. Etiam arcu eros, hendrerit ut placerat ac, cursus vitae urna. Nulla non nisl at leo ornare consequat in vitae augue. Donec vehicula massa tempus venenatis consectetur. Etiam fringilla purus vel ultricies rutrum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere euismod ante, a fringilla purus faucibus in. "
+
+lorem_OTP = oneTimePadEncrypt(loremIpsum,testKey2)
+print(lorem_OTP)
+print(oneTimePadDecrypt(lorem_OTP,testKey2))
